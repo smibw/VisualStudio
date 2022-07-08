b1eed5c7335ed23a8d3804d9c1357fb8cbdf52a7
Кирилл Сазыкин <zzzkiril@mail.ru>
2022-07-08 19:36:38 +0300

    fuctions

---
+++ repos/Functions/Functions/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
+
+#define tab "\t"
+#define delimiter "\n---------------------\n"
+
+void main()
+{
+	setlocale (LC_ALL, "");
+
+}
