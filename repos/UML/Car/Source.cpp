8fe57c537c75646b2e22d08b45c2d8972b95f6b9
Кирилл Сазыкин <zzzkiril@mail.ru>
2023-01-10 21:30:09 +0300

    WinAPI

---
+++ repos/UML/Car/Source.cpp
@@ -0,0 +1,207 @@
+﻿#include <iostream>
+#include<conio.h>
+#include<thread>
+using namespace std;
+using namespace std::chrono_literals;
+using std::cout;
+using std::cin;
+using std::endl;
+
+#define delimiter "\n-------------------------------\n"
+#define min_tank_VOLUME 20
+#define max_tank_VOLUME 80
+
+class Tank
+{
+	const unsigned int VOLUME;	//Характеристика объекта
+	double fuel;				//Состояние объекта
+public:
+	unsigned int get_VOLUME()const
+	{
+		return VOLUME;
+	}
+	double get_fuel_level()const
+	{
+		return fuel;
+	}
+	void fill(double fuel)
+	{
+		if (fuel < 0)return;
+		if (this->fuel + fuel < VOLUME)this->fuel += fuel;
+		else this->fuel = VOLUME;
+	}
+	Tank(unsigned int volume)
+		:VOLUME(volume<=min_tank_VOLUME?min_tank_VOLUME:
+			volume>max_tank_VOLUME?max_tank_VOLUME:volume)
+	{
+		this->fuel = 0;
+		std::cout << "Tank is ready" << std::endl;
+	}
+	~Tank()
+	{
+	std::cout << "Tank is over" << std::endl;
+	}
+	void info()const
+	{
+		std::cout << "Tank volume: " << VOLUME << " liters" << std::endl;
+		std::cout << "Fuel level: " << fuel << " liters" << std::endl;
+	}
+};
+
+#define min_engine_consumption 4
+#define max_engine_consumption 20
+
+class Engine
+{
+	double consumption;
+	double consumption_per_second;
+	bool is_started;
+public:
+	double get_conssumption()const
+	{
+		return consumption;
+	}
+	double get_consumption_per_second()const
+	{
+		return consumption_per_second;
+	}
+	Engine(double consumption)
+		:consumption(consumption<min_engine_consumption ? min_engine_consumption :
+			consumption>max_engine_consumption ? max_engine_consumption : consumption)
+	{
+		consumption_per_second = consumption * 3e-5;
+		std::cout << "Engine is ready" << std::endl;
+	}
+	~Engine()
+	{
+		std::cout << "Engine is over" << std::endl;
+	}
+	bool start()
+	{
+		return is_started = true;
+	}
+	bool stop()
+	{
+		return is_started = false;
+	}
+	bool started()const
+	{
+		return is_started;
+	}
+	void info()const
+	{
+		std::cout << "Consumption/100 km: " << consumption<<" liters" << std::endl;
+		std::cout << "Consumption/1sec km: " << consumption_per_second <<" liters" << std::endl;
+		std::cout << "Engine is " << (is_started ? "started" : "stopped") << std::endl;
+	}
+};
+
+class Car
+{
+	Engine engine;
+	Tank tank;
+	bool driver_inside;
+	struct Control
+	{
+		std::thread panel_thread;
+	}threads;
+public:
+	Car(double consumption, int volume):engine(consumption), tank(volume)
+	{
+		driver_inside = false;
+		std::cout << "Your car is ready to go" << std::endl;
+	}
+	~Car()
+	{
+		std::cout << "Your car is over" << std::endl;
+	}
+	void get_in()
+	{
+		driver_inside = true;
+		threads.panel_thread = std::thread(&Car::panel,this);
+	}
+	void get_out()
+	{
+		driver_inside = false;
+		if (threads.panel_thread.joinable()) threads.panel_thread.join();
+		system("CLS");
+		std::cout << "You are out of a car" << std::endl;
+	}
+
+	void control()
+	{
+		char key;
+		do
+		{
+			key = _getch();
+			switch (key)
+			{
+			case 13:
+				if (driver_inside)get_out();
+				else get_in();
+				break;
+			case'F':
+			case'f':
+				if (driver_inside)
+				{
+					std::cout << "Чтобы заправиться, выйдите из машины" << std::endl;
+				}
+				else
+				{
+					double fuel;
+					std::cout << "Введите объем топлива: "; std::cin >> fuel;
+					tank.fill(fuel);
+				}
+					break;
+			case'I':
+			case'i':
+				//if (engine.started())engine.stop();
+				//else engine.start();
+			case 27:
+				get_out();
+				std::cout << "Good buy;-)" << std::endl;
+				break;
+			}
+		} while (key != 27);
+	}
+	void panel()
+	{
+		while (driver_inside)
+		{
+			system("CLS");
+			std::cout << "Engine is " << (engine.started() ? "started" : "stopped") << std::endl;
+			std::cout << "Fuel level: " << tank.get_fuel_level() << " liters" << std::endl;
+			std::this_thread::sleep_for(500ms);
+		}
+	}
+	void info()const
+	{
+		engine.info();
+		tank.info();
+	}
+};
+
+//#define TANK_CHECK
+//#define ENGINE_CHECK
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+#ifdef TANK_CHECK
+	Tank tank(3);
+	tank.info();
+	std::cout << delimiter << std::endl;
+	tank.fill(10);
+	tank.fill(3);
+	tank.info();
+#endif // TANK_CHECK
+
+#ifdef ENGINE_CHECK
+	Engine engine(10);
+	//engine.start();
+	engine.info();
+#endif // ENGINE_CHECK
+
+	Car BMW(12, 60);
+	//BMW.info();
+	BMW.control();
+}
---
+++ repos/WinApi/WinApi/Source.cpp
@@ -0,0 +1,90 @@
+﻿/*
+	Windows API
+	API-Application Programming Interface
+		(Прикладной интерфейс программиста)
+	это набор функицй и других готовых инструментов для разработки приложений
+*/
+#include<Windows.h>
+#include "resource.h"
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+//#define MSG_BOX
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+// INT - тип возвращаемого значения 
+// WINAPI - соглашение о вызове функции(Calling convention)
+// Calling convention определяет какая функция очищает стек, вызывающая или вызываемая,
+// а также определяет, как параметры передаются в функцию
+// для обычных C/C++ приложений по умолчанию используются конвенция __cdecl,
+// в WinApi всегда используется конвенция __stdcall
+// caller - вызывающая функция(функция, которая вызывает другую функцию)
+// callee - вызываемая функция(функция, которую вызывают)
+{
+	/*
+		hInstance - *.exe-файл программы
+		hPrevInst - прудыдущий экзепляр программы
+		lpCmdLine - командная строка, которую приложение получает при запуске.
+					Если это текстовый редактор, то в него можно передать открываемый файл,
+					если это браузер, то в него можно передать загружаемую страницу и т.д.
+					Командная строка представляет собой массив строк, нулевым элементом которого
+					всегда является имя *.exe-файла.
+					lp - long pointer(Венгерская нотация)
+		nCmdShow -	режим отображения окна при запуске(развернуто на весь экран, свернуто в окно,
+					свернуто на панель задач и т.д.)
+					n - number (Венгерская нотация)
+	*/
+#ifdef MSG_BOX
+	MessageBox(
+		NULL,
+		"Привет! Это наше первое сообщение",
+		"Заголовок окна сообщения",
+		MB_YESNOCANCEL | MB_HELP |  // Набор кнопок
+		MB_ICONINFORMATION |		 // Значок
+		MB_DEFBUTTON3 |			 // Кнопка по умолчанию
+		MB_SYSTEMMODAL);		 // WS_EX_TOPMOST - поверх всех окон
+	// IDYES IDNO  
+#endif // MSG_BOX
+
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
+
+	return 0;
+}
+/*
+	У каждого окна есть своя процедура окна - это функция, которая вызывается при запуске окна
+	и обрабатывает все действия связанные с этим окном.
+	Любая процедура окна принимает 4 параметра:
+	1. Окно
+	2. Сообщение
+	3, 4 Параметры сообщения
+	Одна и та же процедура окна может использоваться сразу для нескольких окон
+
+*/
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+	{
+		HICON hicon = LoadIcon(GetModuleHandle(NULL), (LPSTR)IDI_ICON1);
+		//SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+	}
+		break;					// нужен для создания элементов окна
+	case WM_COMMAND:			// Обработка комманд нажатия кнопок
+		switch (LOWORD(wParam))
+		{
+		case IDOK:MessageBox(NULL, "Была нажата кнопка ОК", "Info", MB_OK | MB_ICONINFORMATION); break;
+		case IDCANCEL:EndDialog(hwnd, 0);
+		}
+		break;		
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+	}
+	return FALSE;
+}
+/*
+	1. hwnd - окно
+	2. uMsg - сообщение
+	3. wParam - Параметры сообщения
+	4. lParam
+*/
