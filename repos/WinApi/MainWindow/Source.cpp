f6a1e3b9b24187d2a0d63771ef9687264b1f4766
Кирилл Сазыкин <zzzkiril@mail.ru>
2023-01-28 12:52:07 +0300

    sector clear

--- repos/WinApi/MainWindow/Source.cpp
+++ repos/WinApi/MainWindow/Source.cpp
@@ -2,6 +2,13 @@
 #include<stdio.h>
 #include<Windows.h>
 
+CONST INT g_INTERVAL = 5;
+CONST INT g_BUTTON_SIZE = 50;
+CONST INT g_DISPLAY_WIDTH = 400;
+CONST INT g_DISPLAY_HEIGHT = 20;
+CONST INT g_START_X = 10;
+CONST INT g_START_Y = 10;
+
 CONST CHAR gsz_MY_WINDOW_CLASS[] = "MyWindowClass";
 CONST CHAR gsz_WINDOW_NAME[] = "My First Window";
 
@@ -33,9 +40,11 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	//2) Создание окна
 
 	int screen_width = GetSystemMetrics(SM_CXSCREEN);
-	int screen_height = GetSystemMetrics(SM_CXSCREEN);
-	int window_width = screen_width * .75;
-	int window_height = screen_width * .75;
+	int screen_height = GetSystemMetrics(SM_CYSCREEN);
+	//int window_width = screen_width * 3 / 4;
+	//int window_height = screen_width * 3 / 4;
+	int window_width = g_START_X * 3.5 + (g_BUTTON_SIZE + g_INTERVAL) * 5;
+	int window_height = g_START_Y + (g_BUTTON_SIZE + g_INTERVAL) * 4 + g_INTERVAL * 2 + 55;
 	int start_x = screen_width / 8;
 	int start_y = screen_height / 8;
 
@@ -44,11 +53,12 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		NULL,	//ExStyle
 		gsz_MY_WINDOW_CLASS,	//Class name
 		gsz_WINDOW_NAME,		//Window name
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT,	//нАЧАЛЬНАЯ ПОЗИЦИЯ ОКНА
-		CW_USEDEFAULT, CW_USEDEFAULT,	//РАЗМЕР ОКНА
-		NULL,
-		NULL,
+		//WS_OVERLAPPEDWINDOW,	//dwStyle
+		WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,	//dwStyle
+		start_x, start_y,	//Начальная позиция окна
+		window_width, window_height,	//Размер окна
+		NULL,		//HWND родительского окна. У главного окна НЕТ родительского окна
+		NULL,		//Menu отсутствует
 		hInstance,
 		NULL
 	);
@@ -80,21 +90,24 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		RECT rect;
 		GetWindowRect(hwnd, &rect);
 		CONST INT SIZE = 256;
-		CHAR sz_buffer[SIZE];
-		sprintf(sz_buffer, "&S, Size: %i:%i, Position %i:%i", rect.left,rect.top, rect.right-rect.left,rect.bottom-rect.top);
+		CHAR sz_buffer[SIZE] = {};
+		sprintf(sz_buffer, "%s, Position:%dx%d, Size:%dx%d", gsz_WINDOW_NAME,
+			rect.left, rect.top,
+			rect.right - rect.left, rect.bottom - rect.top);
+		SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_buffer);
 	}		
 		break;
-	case WM_DESTROY:PostQuitMessage(0); break;
+	case WM_DESTROY: PostQuitMessage(0); break;
 	case WM_CLOSE:
 		switch (MessageBox(hwnd, "Вы действительно хотите закрыть окно", "Мне очень надо получить ответ", MB_YESNO | MB_ICONQUESTION))
 		{
 		case IDYES:
-			DestroyWindow(hwnd);
 			MessageBox(hwnd, "Проваливай", "Даже окно закрыть не можешь нормально", MB_OK | MB_ICONINFORMATION);
 			DestroyWindow(hwnd);
 			break;
-		break;
 		}
+		DestroyWindow(hwnd);
+		break;
 	default:return DefWindowProc(hwnd, uMsg, wParam, lParam);
 	}
 	return 0;
