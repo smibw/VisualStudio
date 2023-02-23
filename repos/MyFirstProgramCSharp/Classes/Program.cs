f5abd145a64ddc92dc04d05d9f928fe2116c9c92
Кирилл Сазыкин <zzzkiril@mail.ru>
2023-02-23 17:20:30 +0300

    code from lesson

--- repos/MyFirstProgramCSharp/Classes/Program.cs
+++ repos/MyFirstProgramCSharp/Classes/Program.cs
@@ -8,7 +8,42 @@ using static System.Console;
 namespace Classes
 {
 
+    class Mathematic
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Sum(int a, int b, int c)
+        {
+            return a + b + c;
+        }
 
+        public static double Sum(double da, double db)
+        {
+            return da +db;
+        }
+    }
+    /*
+    class Student
+    {
+        static string _academyName = "Компьютерная академия \"ТОП\"";
+        string _firstName = "Анатолий";
+
+        public void ShowName()
+        {
+            WriteLine(_firstName);
+        }
+
+        public static void ShowAcademy()
+        {
+            WriteLine(_academyName);
+        }
+
+    }
+    */
+    //---------------------------------------------------------------------
+    /*
     public class ClassA
     {
 
@@ -66,11 +101,19 @@ namespace Classes
         }
             
     }
+    */
     internal class Classes
     {
+        /*
+        static void MyFunction(int[] MyArr, int i)
+        {
+            MyArr[0] = 100;
+            i = 100;
+        }
+        */
         static void Main(string[] args)
         {
-
+            /*
             ClassA a=new ClassA();
             ClassB b = new ClassB();
             a.MethodA(b);
@@ -81,6 +124,27 @@ namespace Classes
                 myCar.SpeedUp(15);
                 myCar.PrintState();
             }
+            */
+            /*
+            Student student1 = new Student();
+            student1.ShowName();
+            Student.ShowAcademy();
+
+            int i = 0;
+            int[] MyArr = { 0, 1, 2, 3, 4 };
+            WriteLine("i= " + i);
+            WriteLine("MyArr[0] = " + MyArr[i]);
+            WriteLine("Выозов MyFunction");
+            MyFunction(MyArr, i);
+            WriteLine("i = " + i);
+            WriteLine("MyArr[0] = " + MyArr[0]);
+            */
+
+            int a = 10, b = 20, c = 30;
+            double da = 3.3, db = 6.7;
+            WriteLine($"{a}+{b} = {Mathematic.Sum(a,b)}");
+            WriteLine($"{a}+{b}+{c} = {Mathematic.Sum(a,b,c)}");
+            WriteLine($"{da}+{db} = {Mathematic.Sum(da,db)}");
         }
     }
 }
